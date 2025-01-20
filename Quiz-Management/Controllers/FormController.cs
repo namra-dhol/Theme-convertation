dc28b99a8d7f60a5ac4d0cf1e0d01c32880a8d0d
namra-dhol <dholnamra@gmail.com>
2025-01-20 11:12:44 +0530

    20-01-2025

---
+++ Quiz-Management/Controllers/FormController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Quiz_Management.Controllers
+{
+    public class FormController : Controller
+    {
+        public IActionResult FormElement()
+        {
+            return View();
+        }
+
+        public IActionResult FormLayout()
+        {
+            return View();
+        }
+
+        public IActionResult FormEditor()
+        {
+            return View();
+        }
+
+        public IActionResult FormValidation()
+        {
+            return View();
+        }
+
+
+    }
+}
