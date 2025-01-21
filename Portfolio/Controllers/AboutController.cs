3ebb8d1540a2fc727f541a4508c1f498e5432979
namra-dhol <dholnamra@gmail.com>
2025-01-21 11:07:41 +0530

    theme converation portfolio

---
+++ Portfolio/Controllers/AboutController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Portfolio.Controllers
+{
+    public class AboutController : Controller
+    {
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Hero()
+        {
+            return View();
+        }
+    }
+}
---
+++ Portfolio/ReadME.txt
@@ -0,0 +1,6 @@
+hy this is theme converation 
+Template Name: Laura
+Template URL: https://bootstrapmade.com/laura-free-creative-bootstrap-theme/
+Author: BootstrapMade.com
+License: https://bootstrapmade.com/license/
+
