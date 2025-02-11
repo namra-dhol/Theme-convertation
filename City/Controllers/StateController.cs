f92c6682f0aea88a2c11a83f18e97b079bb388da
namra-dhol <dholnamra@gmail.com>
2025-02-11 19:52:22 +0530

    add areademo

---
+++ City/Controllers/StateController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace City.Controllers
+{
+    public class StateController : Controller
+    {
+        public IActionResult State()
+        {
+            return View();
+        }
+    }
+}
---
+++ City/Models/StateModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace City.Models
+{
+    public class StateModel
+    {
+        [Required]
+        public string Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+    }
+}
